cd519a4042f6fccb8d358546db41a11b8fef47f6
wafaa mohamed <120178783+wafaamohamed1@users.noreply.github.com>
2024-06-07 07:16:08 -0400

    Create 2-binary_tree_insert_right.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_insert_right - function
+ * @parent: parent
+ * @value: valaue
+ * Return: none
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
