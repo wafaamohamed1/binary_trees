3911cffb6785007e773b0cb8325ede6c431bd756
wafaa mohamed <120178783+wafaamohamed1@users.noreply.github.com>
2024-06-07 07:18:13 -0400

    Create 6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_preorder - function
+ * @tree: parent
+ * @func: ptr
+ * Return: none
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	if (tree == NULL || func == NULL)
+		return;
+	func(tree->n);
+	if (tree->left != NULL)
+		binary_tree_preorder(tree->left, func);
+	if (tree->right != NULL)
+		binary_tree_preorder(tree->right, func);
+
+}
