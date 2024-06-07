e3dbf1def5352b2106eb8fdb22d6dfe6e91d83dc
wafaa mohamed <120178783+wafaamohamed1@users.noreply.github.com>
2024-06-07 07:15:30 -0400

    Create 1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,28 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_insert_left - function
+ * @parent: parent
+ * @value: valaue
+ * Return: none
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (new_node);
+}
