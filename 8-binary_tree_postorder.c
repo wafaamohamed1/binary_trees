84dfe65751eb32112807e157ab09f52663e7fa9a
wafaa mohamed <120178783+wafaamohamed1@users.noreply.github.com>
2024-06-07 07:19:26 -0400

    Create 8-binary_tree_postorder.c

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_postorder - function
+ * @tree: parent
+ * @func: ptr
+ * Return: none
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	if (tree == NULL || func == NULL)
+		return;
+	if (tree->left != NULL)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right != NULL)
+		binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
